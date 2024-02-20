2e6bea9135d8d744db74d03e1fad384eecdce7af
bendn <bend.n@outlook.com>
2024-02-20 07:02:04 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# beeg number conversion
+
+ever been confused when somebody says `1 vigintillion` and you have no idea how many zeroes there are?
+```bash
+$ beeg 1 vigintillion
+1e+63
+```
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::process::ExitCode;
+
+static TAB: phf::Map<&str, u16> = phf::phf_map! {
+    "m" => 6,
+    "b" => 9,
+    "tr" => 12,
+    "quad" => 15,
+    "quint" => 18,
+    "sext" => 21,
+    "sept" => 24,
+    "oct" => 27,
+    "non" => 30,
+    "dec" => 33,
+    "undec" => 36,
+    "duodec" => 39,
+    "tredec" => 42,
+    "quattourdec" => 45,
+    "quindec" => 48,
+    "sexdec" => 51,
+    "septendec" => 54,
+    "octodec" => 57,
+    "novemdec" => 60,
+    "vigint" => 63,
+    "cent" => 303,
+};
+
+fn main() -> ExitCode {
+    let re = regex::Regex::new(r"([0-9]+(?:\.[0-9]+)?)[^\S]*(m|b|tr|quad|quint|sext|sept|oct|non|dec|undec|duodec|tredec|quattourdec|quindec|sexdec|septendec|octodecc|novemdec|vigint|cent)illion").unwrap();
+    let Some(x) = std::env::args().skip(1).reduce(|acc, x| acc + &x) else {
+        comat::cprintln!("{red}require argument{reset}");
+        return ExitCode::FAILURE;
+    };
+
+    let Some(mat) = re.captures(&x) else {
+        comat::cprintln!("{red}wher num?{reset}");
+        return ExitCode::SUCCESS;
+    };
+    let exponent = TAB[mat.get(2).unwrap().into()];
+    println!("{}e+{exponent}", mat.get(1).unwrap().as_str());
+    ExitCode::SUCCESS
+}
